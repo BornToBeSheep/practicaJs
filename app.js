4c23412fee78531415ca5df8620771e9c34c5c8a
Nahuel Lombardi <lombardi81093@gmail.com>
2022-08-31 23:39:24 -0300

    Se realizan cambios en el repositorio para ajustar el proyecto a la segunda entrega

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-let decimal = parseInt(prompt("ingrese el numero a analizar"));
-let bits = parseInt(prompt("Ingrese la cantidad de bits"));
 
-if (decimal>= 0 && decimal <= Math.pow(2, bits - 1)){
-    let digito;
-    for (i = 0; i <= bits - 1; i++){
-        digito = parseInt(decimal % 2); 
-        decimal = decimal / 2;
-        alert (digito);
+function seguirComprando () {
+    let confirmacion = prompt("Desea seguir comprando? Ingrese S/N");
+    if (confirmacion == "S"){
+        confirmacion = true;
+        return confirmacion;
+    }else if (confirmacion == "N"){
+        return false;
     }
-}else {
-    alert (`El numero ingresado no se puede expresar con ${bits} bits`)
 }
+
+function sacarPromedio (total, cont){
+    return total / cont;
+}
+
+let valor = 0;
+let cont = 0;
+let total = 0;
+do{
+    let agregar = parseInt(prompt("Ingrese el numero del producto que desea agregar al carrito"));
+    let valor = Math.ceil(Math.random()*30);   
+    total = total + valor;
+    cont++;
+}while (seguirComprando());
+alert (`El costo total de lo comprado es: ${total}`);
+alert (`El promedio de lo comprado es: ${sacarPromedio(total, cont)}`);
